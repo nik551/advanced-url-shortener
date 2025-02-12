5c7a8caddd0f793ef5b71e3fa208b0db0cbb8aa9
nik551 <nikhil.varghese23@gmail.com>
2025-02-12 10:34:01 +0530

    removing mongodb-memory-server

--- tests/setup.js
+++ tests/setup.js
@@ -1,18 +1,17 @@
 const mongoose = require("mongoose");
-const { MongoMemoryServer } = require("mongodb-memory-server");
 const client = require("../config/redis");
 
 
-let mongoServer;
+//let mongoServer;
 
-beforeAll(async () => {
-  mongoServer = await MongoMemoryServer.create();
-  const uri = mongoServer.getUri();
-  await mongoose.createConnection(uri, {});
-});
+// beforeAll(async () => {
+//   mongoServer = await MongoMemoryServer.create();
+//   const uri = mongoServer.getUri();
+//   await mongoose.createConnection(uri, {});
+// });
 
 afterAll(async () => {
   await mongoose.disconnect();
-  await mongoServer.stop();
+  //await mongoServer.stop();
   await client.quit();
 });
